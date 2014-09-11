74f19d73c3a653dc314f3bfb3c972fbbbb223244
Simon Cropp <simon.cropp@gmail.com>
2014-09-12 09:58:50 +1000

    better doco

--- src/NServiceBus.Log4Net/Log4NetFactory.cs
+++ src/NServiceBus.Log4Net/Log4NetFactory.cs
@@ -4,7 +4,7 @@
     using NServiceBusLog4Net;
 
     /// <summary>
-    /// Configure NServiceBus logging messages to use Log4Net.
+    /// Configure NServiceBus logging messages to use Log4Net. Use by calling <see cref="LogManager.Use{T}"/> the T is <see cref="Log4NetFactory"/>.
     /// </summary>
     public class Log4NetFactory: LoggingFactoryDefinition
     {
