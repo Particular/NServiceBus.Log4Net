3be431cda09e90dcf96ec80492a8f0211ae97d8c
Simon Cropp <simon.cropp@gmail.com>
2014-09-30 21:58:23 +1000

    remove redundant file

--- src/Sample/Program.cs
+++ src/Sample/Program.cs
@@ -20,6 +20,7 @@ class Program
         using (var bus = Bus.Create(busConfig))
         {
             bus.Start();
+            bus.SendLocal(new MyMessage());
             Console.WriteLine("\r\nPress any key to stop program\r\n");
             Console.Read();
         }
