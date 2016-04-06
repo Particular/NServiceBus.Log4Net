a1c61e3aba7c09222572a52634783cbf1ea88f7d
Simon Cropp <simon.cropp@gmail.com>
2016-04-06 17:21:35 +1000

    update to latest core

--- src/NServiceBus.Log4Net/AssemblyInfo.cs
+++ src/NServiceBus.Log4Net/AssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Reflection;
 
 [assembly: AssemblyTitle("NServiceBus Log4Net integration")]
 [assembly: AssemblyDescription("Implementation of logging functionality on top of Log4Net.")]
-[assembly: AssemblyCopyright("Copyright 2010-2014 NServiceBus. All rights reserved")]
+[assembly: AssemblyCopyright("Copyright 2010-2016 NServiceBus. All rights reserved")]
 [assembly: AssemblyProduct("NServiceBus")]
 [assembly: AssemblyCompany("NServiceBus Ltd.")]
 [assembly: CLSCompliant(true)]
